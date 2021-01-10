9ea533e76ce18a5291ae56d1dc8dfa2f579f368a
infret <infr3t@gmail.com>
2021-01-10 17:42:56 +0000

    Finish job cards

--- src/components/Job.jsx
+++ src/components/Job.jsx
@@ -1,18 +1,23 @@
 import {Badge, Card} from 'react-bootstrap'
+import Parse from 'html-react-parser'
 
-export default function Job({job}){
+export default function Job({job}) {
 	return (
 			<Card>
 				<Card.Body>
 					<div className='d-flex justify-content-between'>
-					<Card.Title>
-						{job.title} - <span className='text-muted font-weight-light'>{job.company}</span>
-					</Card.Title>
-						<Card.Subtitle className='text-muted mb-2'>
-							{new Date(job.created_at).toLocaleDateString()}
-						</Card.Subtitle>
-						<Badge>{job.type}</Badge>
-						<Badge>{job.location}</Badge>
+						<div>
+							<Card.Title>
+								{job.title} - <span className='text-muted font-weight-light'>{job.company}</span>
+							</Card.Title>
+							<Card.Subtitle className='text-muted mb-2'>
+								{new Date(job.created_at).toLocaleDateString()}
+							</Card.Subtitle>
+							<Badge variant='secondary' className='mr-2'>{job.type}</Badge>
+							<Badge variant='secondary'>{job.location}</Badge>
+							<div style={{wordBreak: 'break-all'}}>{Parse(job.how_to_apply)}</div>
+						</div>
+						<img className='d-none d-md-block' height='50' alt={job.company} src={job.company_logo}/>
 					</div>
 				</Card.Body>
 			</Card>
