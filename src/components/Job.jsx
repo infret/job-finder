67503055d6a613c48ee87b593c791f454f8c0411
infret <infr3t@gmail.com>
2021-01-10 11:39:57 +0000

    Fix fetching

--- src/components/Job.jsx
+++ src/components/Job.jsx
@@ -1,12 +1,19 @@
-import {Card} from 'react-bootstrap'
+import {Badge, Card} from 'react-bootstrap'
 
 export default function Job({job}){
 	return (
 			<Card>
 				<Card.Body>
+					<div className='d-flex justify-content-between'>
 					<Card.Title>
-					{job.title}
+						{job.title} - <span className='text-muted font-weight-light'>{job.company}</span>
 					</Card.Title>
+						<Card.Subtitle className='text-muted mb-2'>
+							{new Date(job.created_at).toLocaleDateString()}
+						</Card.Subtitle>
+						<Badge>{job.type}</Badge>
+						<Badge>{job.location}</Badge>
+					</div>
 				</Card.Body>
 			</Card>
 	)
--- src/useFetchJobs.js
+++ src/useFetchJobs.js
@@ -1,55 +1,58 @@
 import {useEffect, useReducer} from 'react'
-import axios from 'axios'
 
 const ACTIONS = {
 	MAKE_REQUEST: 'make-request',
 	GET_DATA: 'get-data',
-	ERROR: 'error'
+	ERROR: 'error',
 }
 
-function reducer(state, action){
+function reducer(state, action) {
 	switch (action.type) {
 		case ACTIONS.MAKE_REQUEST:
 			return {
 				loading: true,
-				jobs: []
+				jobs: [],
 			}
 		case ACTIONS.GET_DATA:
 			return {
 				...state,
 				loading: false,
-				jobs: action.payload.jobs
+				jobs: action.payload.jobs,
 			}
 		case ACTIONS.ERROR:
 			return {
 				...state,
 				loading: false,
 				error: action.payload.error,
-				jobs: []
+				jobs: [],
 			}
 		default:
 			return state
 	}
 }
 
-export default function useFetchJobs(params, page){
+export default function useFetchJobs(params, page) {
 	const [state, dispatch] = useReducer(reducer, {jobs: [], loading: true})
 
 	useEffect(() => {
-		const cancelToken = axios.CancelToken.source()
-		dispatch({type: ACTIONS.MAKE_REQUEST})
-		axios.get('https://cors-anywhere.herokuapp.com/https://jobs.github.com/positions.json', {
-			cancelToken: cancelToken.token,
-			params: {markdown: true, page: page, ...params }
-		}).then(res => {
-			dispatch({type: ACTIONS.GET_DATA, payload: {jobs: res.data}})
-	}).catch(e => {
-		if (axios.isCancel(e)) return
-		dispatch({type: ACTIONS.ERROR, payload: {error: e}})
-		})
+		const controller = new AbortController()
+		fetch('https://cors-anywhere.herokuapp.com/https://jobs.github.com/positions.json', {
+			method: 'GET',
+			signal: controller.signal,
+			params: {
+				markdown: true,
+				page: page,
+				...params,
+			},
+		}).then(res => res.json()).then(data => {
+					dispatch({type: ACTIONS.GET_DATA, payload: {jobs: data}})
+				},
+		).catch(e =>
+				dispatch({type: ACTIONS.ERROR, payload: {error: e}}),
+		)
 
 		return () => {
-			cancelToken.cancel()
+			controller.abort()
 		}
 	}, [params, page])
 
