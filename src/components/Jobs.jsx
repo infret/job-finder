d0ec2f8efb327cf2d2341f437bfc3f017783d9e9
infret <infr3t@gmail.com>
2021-01-29 23:53:05 +0000

    Improve header area

--- src/components/Jobs.jsx
+++ src/components/Jobs.jsx
@@ -1,5 +1,12 @@
 import useFetchJobs from '../useFetchJobs'
-import { Container, Navbar, Nav, Image, Carousel } from 'react-bootstrap'
+import {
+  Container,
+  Navbar,
+  Nav,
+  Image,
+  Carousel,
+  Button
+} from 'react-bootstrap'
 import Job from './Job'
 import { useState } from 'react'
 import Pagination from './Pagination'
@@ -19,44 +26,44 @@ export default function Jobs() {
   }
   return (
     <>
-      <Navbar bg='white' expand='lg'>
-        <Navbar.Brand href='#home'>
-          <BriefcaseFill size={26} color='text-primary' className='mr-2 mb-1' />
+      <Navbar
+        expand='lg'
+        className='position-absolute p-5 w-100'
+        style={{ zIndex: '1000' }}
+      >
+        <Navbar.Brand href='#home' className='text-white'>
+          <BriefcaseFill size={26} color='white' className='mr-2 mb-1' />
           Job Finder
         </Navbar.Brand>
         <Navbar.Toggle aria-controls='basic-navbar-nav' />
         <Navbar.Collapse id='basic-navbar-nav'>
-          <Nav className='mr-auto'>
-            <Nav.Link href='#home'>Home</Nav.Link>
-            <Nav.Link href='#'>For seekers</Nav.Link>
-            <Nav.Link href='#'>For employers</Nav.Link>
+          <Nav className='w-100'>
+            <Nav.Link href='#' className='text-white'>
+              Home
+            </Nav.Link>
+            <Nav.Link href='#' className='text-white'>
+              For seekers
+            </Nav.Link>
+            <Nav.Link href='#' className='text-white'>
+              For employers
+            </Nav.Link>
+            <Button className='ml-auto btn-outline-white bg-transparent'>
+              Login
+            </Button>
+            <Button className='ml-3 bg-transparent'>Register</Button>
           </Nav>
         </Navbar.Collapse>
       </Navbar>
-      <Carousel>
-        <Carousel.Item>
-          <img
-            className='vh-100 w-100'
-            style={{ objectFit: 'cover' }}
-            src='https://images.unsplash.com/photo-1454165804606-c3d57bc86b40?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=750&q=80'
-            alt=''
-          />
-          <Carousel.Caption className='mb-5'>
-            <h1 className='mb-4'>Find your dream job</h1>
-          </Carousel.Caption>
-        </Carousel.Item>
-        <Carousel.Item>
-          <img
-            className='vh-100 w-100'
-            style={{ objectFit: 'cover' }}
-            src='https://images.unsplash.com/photo-1513430698680-03ff4d6be961?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80'
-            alt=''
-          />
-          <Carousel.Caption className='mb-5'>
-            <h1 className='mb-4'>Find your dream job</h1>
-          </Carousel.Caption>
-        </Carousel.Item>
-      </Carousel>
+      <div
+        className='vh-100 w-100'
+        style={{
+          filter: 'brightness(30%)',
+          backgroundSize: 'cover',
+          backgroundRepeat: 'no-repeat',
+          backgroundImage:
+            'url(https://images.unsplash.com/photo-1454165804606-c3d57bc86b40?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80)'
+        }}
+      ></div>
       <Container className='my-4'>
         <SearchForm
           params={params}
